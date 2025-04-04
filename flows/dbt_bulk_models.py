17b8e2b52d857af5e58ed3f4dc7d26bacb4371e0
Serguei Ossokine <serguei.ossokine@tutanota.com>
2025-04-04 23:24:20 +0200

    Debugging paths

--- flows/dbt_bulk_models.py
+++ flows/dbt_bulk_models.py
@@ -1,12 +1,15 @@
 from prefect import flow
 from prefect_dbt import PrefectDbtRunner, PrefectDbtSettings
 import os
+from prefect.logging import get_run_logger
 
 pth = os.path.dirname(__file__)
 
 
 @flow
 def build_sensors_per_location(flag):
+    logger = get_run_logger()
+    logger.info(f"I am in {os.getcwd()}")
     PrefectDbtRunner(
         settings=PrefectDbtSettings(
             project_dir=os.path.join(pth, "../dbt/airquality"),
