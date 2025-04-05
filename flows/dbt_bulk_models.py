af3d34b37b76f79a24123b66d60d5497147909a6
Serguei Ossokine <serguei.ossokine@tutanota.com>
2025-04-05 10:56:12 +0200

    Add more logs

--- flows/dbt_bulk_models.py
+++ flows/dbt_bulk_models.py
@@ -10,10 +10,12 @@ pth = os.path.dirname(__file__)
 def build_sensors_per_location(flag):
     logger = get_run_logger()
     logger.info(f"I am in {os.getcwd()}")
+    dbt_path = os.path.join(pth, "../dbt/airquality")
+    logger.info(f"The dbt_path is {dbt_path}")
     PrefectDbtRunner(
         settings=PrefectDbtSettings(
-            project_dir=os.path.join(pth, "../dbt/airquality"),
-            profiles_dir=os.path.join(pth, "../dbt/airquality"),
+            project_dir=dbt_path,
+            profiles_dir=dbt_path,
         )
     ).invoke(["build"])
 
