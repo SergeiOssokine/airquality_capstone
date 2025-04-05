76fe88067aaa555324588e1007ee47372cc5ca73
Serguei Ossokine <serguei.ossokine@tutanota.com>
2025-04-05 11:21:12 +0200

    Pause before running

--- flows/dbt_bulk_models.py
+++ flows/dbt_bulk_models.py
@@ -2,6 +2,7 @@ from prefect import flow
 from prefect_dbt import PrefectDbtRunner, PrefectDbtSettings
 import os
 from prefect.logging import get_run_logger
+import time
 
 pth = os.path.dirname(__file__)
 
@@ -12,12 +13,16 @@ def build_sensors_per_location(flag):
     logger.info(f"I am in {os.getcwd()}")
     dbt_path = os.path.abspath(os.path.join(pth, "../dbt/airquality"))
     logger.info(f"The dbt_path is {dbt_path}")
+    logger.info("Napping for 10 seconds")
+    time.sleep(10)
+    logger.info("Now starting dbt commands")
     runner = PrefectDbtRunner(
         settings=PrefectDbtSettings(
             project_dir=dbt_path,
             profiles_dir=dbt_path,
         )
     )
+
     # First parse and compile to generate manifest
     runner.invoke(["parse"])
     runner.invoke(["compile"])
