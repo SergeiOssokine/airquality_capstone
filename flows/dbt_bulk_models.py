e6af4eda16cd128c831a6b9ee8a4a53631590626
Serguei Ossokine <serguei.ossokine@tutanota.com>
2025-04-05 11:43:28 +0200

    Fix typo

--- flows/dbt_bulk_models.py
+++ flows/dbt_bulk_models.py
@@ -13,7 +13,7 @@ def build_sensors_per_location(flag):
     dbt_path = os.path.abspath(os.path.join(pth, "../dbt/airquality"))
     logger.info(f"The dbt_path is {dbt_path}")
     # Workaround for https://github.com/PrefectHQ/prefect/issues/17555
-    os.environ["DBT_PROJECTS_DIR"] = dbt_path
+    os.environ["DBT_PROJECT_DIR"] = dbt_path
     os.environ["DBT_PROFILES_DIR"] = dbt_path
     runner = PrefectDbtRunner()
 
