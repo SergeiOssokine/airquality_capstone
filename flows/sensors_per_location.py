6170e852150bea334912f9aad68f27640aab2995
Serguei Ossokine <serguei.ossokine@tutanota.com>
2025-04-05 11:58:25 +0200

    Add the whole flow back in

--- flows/sensors_per_location.py
+++ flows/sensors_per_location.py
@@ -1,15 +1,17 @@
+import os
 from prefect import flow
 from dbt_bulk_models import build_sensors_per_location
+from get_sensors_per_location import get_sensors_per_location
 
 
 @flow
 def sensors_per_location():
-    # bucket_name = os.getenv("DATALAKE_BUCKET")
-    # project_id = os.getenv("PROJECT_ID")
-    # dwh = os.getenv("DWH_NAME")
+    bucket_name = os.getenv("DATALAKE_BUCKET")
+    project_id = os.getenv("PROJECT_ID")
+    dwh = os.getenv("DWH_NAME")
 
-    # flag = get_sensors_per_location(project_id, dwh, bucket_name)
-    build_sensors_per_location(True)
+    flag = get_sensors_per_location(project_id, dwh, bucket_name)
+    build_sensors_per_location(flag)
 
 
 if __name__ == "__main__":
