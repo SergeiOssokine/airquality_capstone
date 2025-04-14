e175d85d4f670b1da4e513fe4b5c87b6fba47456
Serguei Ossokine <serguei.ossokine@tutanota.com>
2025-04-14 15:25:06 +0200

    Fix venv name

--- setup/python_env/setup_env.sh
+++ setup/python_env/setup_env.sh
@@ -9,11 +9,11 @@ else
 fi
 
 echo "Setting up the environment with uv"
-uv venv .captsone --python 3.11
-source .captsone/bin/activate
+uv venv .capstone --python 3.11
+source .capstone/bin/activate
 echo "Installing packages"
 uv pip install -r setup/python_env/requirements.txt
 uv pip install --pre prefect-dbt # Done separately, because --pre is not supported on per-package basis
 echo "Installing pre-commit"
 pre-commit install
-echo "Now activate the environment by running source .captsone/bin/activate"
+echo "Now activate the environment by running source .capstone/bin/activate"
