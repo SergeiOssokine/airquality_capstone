5d1e50f5e745b83b0a141b97c64d12c04eb0fb24
Serguei Ossokine <serguei.ossokine@tutanota.com>
2025-04-14 15:24:43 +0200

    Add name of artifact registry repo

--- infra/tf/gcp/artifact_registry.tf
+++ infra/tf/gcp/artifact_registry.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "prefect-images" {
   location = var.gcp_region
-  repository_id = "prefect-images"
+  repository_id = var.artifact_registry_name
   description = "Contains images used for prefect CloudRun jobs"
   format = "DOCKER"
 }
